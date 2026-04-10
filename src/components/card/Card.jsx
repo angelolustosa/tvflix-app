26da1ea0299e67b47d861f0326c3715fef96b85c
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-04-09 22:06:36 -0300

    Criar o card e o useEffect

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <div class="col-6 col-sm-4 col-md-3">
+            <div class="movie-card">
+                <img src="https://media.themoviedb.org/t/p/w440_and_h660_face//pyok1kZJCfyuFapYXzHcy7BLlQa.jpg"
+                    class="card-img-top" alt="Mercy" />
+                <div class="card-body movie-info">
+                    <div class="movie-title">Mercy</div>
+                    <div class="movie-date">2026-01-20</div>
+                </div>
+            </div>
+        </div>
+    ) 
+}
