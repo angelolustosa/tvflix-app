262a8430df601ce67984f73bebddcdadc3342c16
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-04-16 19:21:49 -0300

    Renderizar os card para dada item da list e formatar a data

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { formatDate } from '../../util/DateUtil'
 
-export const Card = () => {
+export const Card = ({ title, date, image }) => {
     return (
-        <div class="col-6 col-sm-4 col-md-3">
-            <div class="movie-card">
-                <img src="https://media.themoviedb.org/t/p/w440_and_h660_face//pyok1kZJCfyuFapYXzHcy7BLlQa.jpg"
-                    class="card-img-top" alt="Mercy" />
-                <div class="card-body movie-info">
-                    <div class="movie-title">Mercy</div>
-                    <div class="movie-date">2026-01-20</div>
+        <div className="col-6 col-sm-4 col-md-3 mb-4">
+            <div className="movie-card">
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${image}`}
+                    className="card-img-top"
+                    alt={title}
+                />
+                <div className="card-body movie-info">
+                    <div className="movie-title">{title}</div>
+                    <div className="movie-date">{date && formatDate(date)}</div>
                 </div>
             </div>
         </div>
-    ) 
+    )
 }
--- src/components/cardlist/CardList.jsx
+++ src/components/cardlist/CardList.jsx
@@ -1,23 +1,26 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from '../card/Card'
 
 export const CardList = () => {
+
+    const [movies, setMovies] = useState([])
 
     const getMoviews = async () => {
         try {
             const config = {
-                //method: 'GET',
                 headers: {
                     accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
+                    Authorization: 'Bearer SEU_TOKEN_AQUI'
                 }
             }
 
-            const response = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', config)
+            const response = await axios.get(
+                'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+                config
+            )
 
-            console.log(response.data.results)
-
-            // a partir do component card , inteirar na lista da api com os dados, utilizando props.
+            setMovies(response.data.results)
 
         } catch (error) {
             console.log(error)
@@ -26,35 +29,21 @@ export const CardList = () => {
 
     useEffect(() => {
         getMoviews()
-
     }, [])
 
-
-
     return (
-        <section class="container mt-5 pt-5" id="carouselSection">
-            <h2 class="section-title">Filmes Populares</h2>
-            <div class="row" id="featuredMovies">
-                <div class="col-6 col-sm-4 col-md-3">
-                    <div class="movie-card">
-                        <img src="https://media.themoviedb.org/t/p/w440_and_h660_face//pyok1kZJCfyuFapYXzHcy7BLlQa.jpg"
-                            class="card-img-top" alt="Mercy" />
-                        <div class="card-body movie-info">
-                            <div class="movie-title">Mercy</div>
-                            <div class="movie-date">2026-01-20</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-sm-4 col-md-3">
-                    <div class="movie-card">
-                        <img src="https://media.themoviedb.org/t/p/w440_and_h660_face//buPFnHZ3xQy6vZEHxbHgL1Pc6CR.jpg"
-                            class="card-img-top" alt="Shelter" />
-                        <div class="card-body movie-info">
-                            <div class="movie-title">Shelter</div>
-                            <div class="movie-date">2026-01-28</div>
-                        </div>
-                    </div>
-                </div>
+        <section className="container mt-5 pt-5">
+            <h2 className="section-title">Filmes Populares</h2>
+
+            <div className="row">
+                {movies.map((movie) => (
+                    <Card
+                        key={movie.id}
+                        title={movie.title}
+                        date={movie.release_date}
+                        image={movie.poster_path}
+                    />
+                ))}
             </div>
         </section>
     )
